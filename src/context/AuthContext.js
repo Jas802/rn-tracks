60257d5bc50f1e5197779622b3285389842d6a59
Jas802 <jas.spencer414@gmail.com>
2021-02-23 13:04:31 -0500

    styled error message for signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,26 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {};
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+    } catch (err) {
+      dispatch({
+        type: 'add_error',
+        payload: 'Something went wrong with sign up',
+      });
+    }
+  };
 };
 
 const signin = (dispatch) => {
@@ -22,5 +34,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, signin, signout },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: '' }
 );
