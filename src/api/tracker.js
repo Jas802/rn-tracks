df212d1ab9fced52391867bb1f902009e17166a6
Jas802 <jas.spencer414@gmail.com>
2021-02-25 14:17:30 -0500

    navigation funciton to nav after signup

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://4d5118c63484.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://8cd7ea70ec27.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
---
+++ src/navigationRef.js
@@ -0,0 +1,16 @@
+import { NavigationActions } from 'reaact-navigation';
+
+let navigator;
+
+export const setNavigator = (nav) => {
+  navigator = nav;
+};
+
+export const navigate = (routeName, params) => {
+  navigator.dispatch(
+    NavigationActions.navigate({
+      routeName,
+      params,
+    })
+  );
+};
