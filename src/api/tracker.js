5c6e1cca23e2dbc437c79ad48990c7064003d04b
Jas802 <jas.spencer414@gmail.com>
2021-03-01 12:07:43 -0500

    fucntions for automatic Signin

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://89fe7d3aee68.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://3a340056355c.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
