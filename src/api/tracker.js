41bddd949821381ae2d8fa0028856031532179ad
Jas802 <jas.spencer414@gmail.com>
2021-03-02 13:29:49 -0500

    styled map component to show location

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://3a340056355c.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://bd76513510f5.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
