66b5e6f48465026b1e9f8f2e6ebd54c7327d9829
Jas802 <jas.spencer414@gmail.com>
2021-03-03 11:04:44 -0500

    mockLocation for testing moving location

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://bd76513510f5.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://2e59670b577b.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
