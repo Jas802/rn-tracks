109bdec937d3344ca482eaee4e31aa8001f143a0
Jas802 <jas.spencer414@gmail.com>
2021-03-04 14:39:38 -0500

    wired locationContext to TrackCreateScreen'

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,14 +11,14 @@ const getLocation = (increment) => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees,
+      longitude: -73.92526418709069 + increment * tenMetersWithDegrees,
+      latitude: 40.768948845404566 + increment * tenMetersWithDegrees,
     },
   };
 };
 
 let counter = 0;
-setInverval(() => {
+setInterval(() => {
   Location.EventEmitter.emit('Expo.locationChanged', {
     watchId: Location._getCurrentWatchId(),
     location: getLocation(counter),
--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://2e59670b577b.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://450df9bfd8f0.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
