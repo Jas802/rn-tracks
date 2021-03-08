b769438832431a447bb1698404c8480d46144bb3
Jas802 <jas.spencer414@gmail.com>
2021-03-08 12:35:01 -0500

    restructured useEffect and useLocation

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,45 +7,42 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(
-    () => {
-      if (shouldTrack) {
-        startWatching();
-      } else {
-        subscriber.remove();
-        setSubscriber(null);
-      }
-      return () => {
-        if (subscriber) {
-          subscriber.remove();
+  useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+        if (!granted) {
+          throw new Error('Location permission not granted');
         }
-      };
-    },
-    [shouldTrack],
-    callback
-  );
+        const subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
+    }
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback]);
 
   return [err];
 };
