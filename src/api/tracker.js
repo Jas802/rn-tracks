058228ff9d2296a403718c117d6e90fa1ee3be43
Jas802 <jas.spencer414@gmail.com>
2021-03-09 12:26:27 -0500

    added button for saving recorded tracks

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://2f83d027f848.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://f3ad066af7ff.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
