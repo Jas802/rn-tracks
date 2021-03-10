6507317b22b982e9f2ba5917b5bf7c3b6491a855
Jas802 <jas.spencer414@gmail.com>
2021-03-10 14:49:51 -0500

    Navigate from trackList to trackDetail

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -15,7 +15,11 @@ const TrackListScreen = ({ navigation }) => {
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('TrackDetail', { _id: item._id })
+              }
+            >
               <ListItem bottomDivider>
                 <ListItem.Content>
                   <ListItem.Title>{item.name}</ListItem.Title>
