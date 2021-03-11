30fdb817a1f8d2796792d17ee9cb6a7fa2284db1
Jas802 <jas.spencer414@gmail.com>
2021-03-11 13:41:01 -0500

    displays track map on track detail

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-  baseURL: 'https://76ed5ce15786.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
+  baseURL: 'https://692f59f8b72a.ngrok.io', //due to using ngrok for free URL must be updated to new address every few hours
 });
 
 instance.interceptors.request.use(
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -9,7 +9,7 @@ const TrackListScreen = ({ navigation }) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Text style={{ fontSize: 48 }}>Your Tracks</Text>
       <FlatList
         data={state}
         keyExtractor={(item) => item._id}
